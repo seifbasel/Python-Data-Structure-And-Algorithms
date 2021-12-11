1c8450c19f1dff3e9702bf82c9129b169cb56e66
seifbasel <seifbasel950@gmail.com>
2021-12-11 11:07:11 +0200

    enter

--- train.py
+++ train.py
@@ -3,46 +3,46 @@ class Node:
         self.data = data
         self.next = next
 
-
 class Queue:
+    
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def isEmpty(self):
-        return self.head == None
-    
-    def enquque(self,data):
+    def enqueue(self,data):
         if self.head==None:
             newnode=Node(data)
             self.head=newnode
-            self.tail=self.head
+            self.tail =self.head
         else:
             newnode = Node(data)
             self.tail.next=newnode
             self.tail=newnode
-            
+
     def dequeue(self):
         data = self.head.data
         self.head = self.head.next
         if self.head == None:
             self.last = None
         return data
-         
+    
+    def isEmpty(self):
+        return self.head == None
+
     def display(self):
         current = self.head
         while current != None:
-                print(current.data, end=" ")
-                current = current.next
-
+            print(current.data, "\n ")
+            current = current.next
+                        
 q =Queue()
-q.enquque(10)
-q.enquque(20)
-q.enquque(30)
-q.enquque(40)
-q.enquque(50)
-q.enquque(60)
-q.enquque(70)
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.enqueue(40)
+q.enqueue(50)
+q.enqueue(60)
+q.enqueue(70)
 q.dequeue()
 q.dequeue()
 q.dequeue()
