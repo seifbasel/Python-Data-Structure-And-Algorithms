219d28ed20fa52587338b7c788f8356ee0af5660
seif basel <seifbasel950@gmail.com>
2022-12-03 15:35:40 +0200

    enter

--- all/data_structure/insertion-sort.py
+++ all/data_structure/insertion-sort.py
@@ -1,9 +1,10 @@
 def insertionsort(list_1):
     # loop throught elements of list
     for i in range (0,len(list_1)):
-        value_to_be_sort=list_1[i]
-        
-        while value_to_be_sort < list_1[i-1] and i>0:
+
+        # value_to_be_sort=list_1[i]
+
+        while list_1[i-1] > list_1[i] and i>0: 
             list_1[i], list_1[i-1] = list_1[i-1], list_1[i]
             i=i-1
 
